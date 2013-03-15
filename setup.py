e965df51729638cb702afe526876f50c45d7fab4
Christian Fobel <christian@fobel.net>
2013-03-15 02:29:48 -0400

    Add package configuration information

--- setup.py
+++ setup.py
@@ -2,7 +2,17 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-setup(
+
+setup(name = "genetic_algorithms",
+    version = "0.0.1",
+    description = "Genetic algorithm library",
+    keywords = "cpuinfo",
+    author = "Christian Fobel",
+    url = "https://github.com/cfobel/python-genetic_algorithms",
+    license = "GPL",
+    long_description = """Currently, only contains a crossover operation""",
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension('genetic_algorithms.cCrossover', ['src/cCrossover.pyx'])]
+    packages = ['genetic_algorithms'],
+    ext_modules = [Extension('genetic_algorithms.cCrossover',
+                             ['src/cCrossover.pyx'])]
 )
