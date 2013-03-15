0822d9a77b967bb339fd71c477deddbe643c6b0b
Christian Fobel <christian@fobel.net>
2013-03-15 00:32:00 -0400

    Run both Cython and Python in profile test

--- tests/profile_test.py
+++ tests/profile_test.py
@@ -1,4 +1,21 @@
-#from genetic_algorithms.cCrossover import run_test
+from datetime import datetime
+from collections import OrderedDict
+
+from genetic_algorithms.cCrossover import run_test as cRun_test
 from genetic_algorithms.crossover import run_test
 
+
+deltas = OrderedDict()
+
+start = datetime.now()
 run_test()
+end = datetime.now()
+deltas['python'] = end - start
+
+start = datetime.now()
+cRun_test()
+end = datetime.now()
+deltas['cython'] = end - start
+
+from pprint import pprint
+pprint([(k, v.total_seconds()) for k, v in deltas.items()])
